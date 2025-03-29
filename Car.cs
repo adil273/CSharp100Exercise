0913f5047dcefb6c293b2a9d9f244977ae85d66f
adil273 <adil.uet11@gmail.com>
2025-03-29 19:46:40 +0500

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,11 @@
+// Car.cs (Separate file for the Car class)
+public class Car
+{
+    public string brand;
+    public int speed;
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Car Brand: {brand}, Speed: {speed} km/h");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello, World!");
+//     }
+// }
+// Program.cs (Where we create objects)
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car();
+        myCar.brand = "Toyota";
+        myCar.speed = 120;
+        myCar.ShowInfo();  // Output: Car Brand: Toyota, Speed: 120 km/h
+    }
+}
+
